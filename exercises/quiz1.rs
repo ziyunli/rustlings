20379d16cf658835d7db6cb8915e0f04f42398ea
Ziyun Li <me@ziyunli.dev>
2022-10-17 17:17:01 -0700

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: i32) -> i32 {
+    if n > 40 {
+        n
+    } else {
+        n * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
